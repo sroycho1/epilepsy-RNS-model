91f136bf930c8f4936ca96fe3b0e60e1451abe2e
morestars <33098474+morestars@users.noreply.github.com>
2019-04-26 18:08:25 -0400

    complete v1

--- lineLength.m
+++ lineLength.m
@@ -1,10 +1,23 @@
 %% Line Length Detection Tool
+% hit = 1 -> seizure detected
+% hit = 0 -> no seizure
+% average will weight most recent ecog data more heavily
 
-function hit = lineLength(ecog,dt)
+function [hit, prevLen] = lineLength(ecog,dt,prevLen)
 
 % dy -> diff(ecog)
 % dx -> dt
 % dLen -> c^2 = a^2 + b^2
-len = sqrt( diff(ecog).^2 + dt^2 ); %pythagorean distance
+dLen = sqrt( diff(ecog).^2 + dt^2 ); % pythagorean distance
 
+totLen = sum(dLen);
+
+if totLen > 1.5*prevLen % modify to change relative threshold
+    hit = 1;
+else
+    hit = 0;
+    prevLen = mean(totLen, prevLen);
+end
+
+end
 
